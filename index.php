a69226421d4bf4b027e83eaa18adca3e0a721dab
Matthew Prado <mp399@afsaccess1.njit.edu>
2017-10-18 18:57:43 -0400

    added <br> tag

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ try {
     $conn = new PDO($dsn, $username, $password);
     echo "Connected successfully<br>";
 } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage().'<br>';
 }
 $conn = null;
 
