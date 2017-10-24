8c1dc9d3127717d31f2d0124a195ddcf6572731b
Matthew Prado <mp399@afsconnect2.njit.edu>
2017-10-24 19:48:00 -0400

    error

--- index.php
+++ index.php
@@ -15,24 +15,23 @@ try {
 }
 $conn = null;
 
-$sql = 'SELECT id
+$sql = "SELECT id
     	FROM accounts
-	LIMIT 5';
-$result = mysql_query($sql);
-	if ($result->num_rows > 0) 
+	LIMIT 5";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) 
 	{
-	echo "<table><tr><th>ID</th><th>Email</th></tr>";
-	while($row = $result->fetch_assoc()) 
-	{
-	echo "<tr><td>".$row['id']."</td><td>".$row['email']."</td></tr>";
-	}
-	echo "</table>";
+        while($row = mysqli_fetch_assoc($result)) 
+		{
+		echo "ID: " . $row["id"]. " - Email: " . $row["email"]. "<br>";
+		}
 	} 
-	else 
+else 
 	{
 	echo "0 results";
 	}
-	$conn->close();
+	mysqli_close($conn);
 
 /*if ($result->num_rows > 0) 
 	{
