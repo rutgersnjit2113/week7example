a76f6b9621dca4494973261451dfa964827d20bc
Matthew Prado <mp399@afsconnect2.njit.edu>
2017-10-24 19:30:36 -0400

    error

--- index.php
+++ index.php
@@ -20,6 +20,21 @@ $sql = 'SELECT id, email
         ORDER BY id
 	LIMIT 5';
 $result = mysql_query($sql);
+	if ($result->num_rows > 0) 
+	{
+	echo "<table><tr><th>ID</th><th>Name</th></tr>";
+        // output data of each row
+	while($row = $result->fetch_assoc()) 
+	{
+	echo "<tr><td>".$row['id']."</td><td>".$row['firstname']."</td></tr>";
+	}
+	echo "</table>";
+	} 
+	else 
+	{
+	echo "0 results";
+	}
+	$conn->close();
 
 /*if ($result->num_rows > 0) 
 	{
