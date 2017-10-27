3dd600b1db2330b33db50a5cd17587383f7fe757
Matthew Prado <mp399@afsconnect2.njit.edu>
2017-10-26 23:12:07 -0400

    final commit

--- index.php
+++ index.php
@@ -1,42 +1,37 @@
 <?php
 
 echo "<h1>PDO demo!</h1>";
+$hostname = 'sql2.njit.edu';
 $username = 'mp399';
 $password = 'QHfl76ID';
-$hostname = 'sql2.njit.edu';
-
-$dsn = "mysql:host=$hostname;dbname=$username";
+$dbname = 'mp399';
 
 try {
-    $conn = new PDO($dsn, $username, $password);
+    $conn = new mysqli($hostname, $username, $password, $dbname);
     echo "Connected successfully<br>";
-} catch(PDOException $e) {
+    } 
+    catch(PDOException $e) 
+    {
     echo "Connection failed: " . $e->getMessage().'<br>';
-}
-$conn = null;
+    }
+$sql = "SELECT id, email  FROM accounts LIMIT 5";
+$result = $conn->query($sql);
+$num_rows = mysqli_num_rows($result);
+echo "Results: ".$num_rows."<br>";
 
-$sql = "SELECT id
-    	FROM accounts
-	LIMIT 5";
-$result = mysql_query($sql, $conn);
-if(!result)
-	{
-    	echo mysql_error();
-	}
-$array = mysql_fetch_assoc($result);
-print_r($array);
+if ($result->num_rows > 0) 
+    {
+    echo "<table border = 1><tr><th>ID</th><th>Email</th></tr>";
+    while($row = $result->fetch_assoc()) 
+    {
+    echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td></tr>";
+    }
+    echo "</table>";
+    } 
+else 
+    {
+    echo "0 results";
+    }
+    $conn->close();
 
-/*if ($result->num_rows > 0) 
-	{
-	echo "<table><tr><th>ID</th><th>Email</th></tr>";
-	while($row = $result->fetch_assoc()) 
-	{
-	echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td></tr>";
-	echo "</table>";
-	} 
-	else 
-	{
-	echo "0 results";
-	}
-	$conn->close();*/
 ?>
